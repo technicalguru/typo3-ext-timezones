67c9fcf1103eeea3fb92efffbefd6c4e7299a4dd
ralph <ralph@fdcf5c44-6d3c-11dc-8539-f79c8e7a170c>
2014-02-12 09:42:59 +0000

    Typo3 6.1.7 release

---
+++ OSS/typo3-plugins/timezones/tags/timezones-1.2.0/ext_emconf.php
@@ -0,0 +1,42 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "timezones"
+# 
+# Auto generated 27-01-2007 00:10
+# 
+# Manual updates:
+# Only the data in the array - anything else is removed by next write
+########################################################################
+
+$EM_CONF[$_EXTKEY] = Array (
+	'title' => 'FE Timezone Library',
+	'description' => 'Manages timezones for website users',
+	'category' => 'plugin',
+	'author' => 'Ralph Schuster',
+	'author_email' => 'rs.eschborn@gmx.de',
+	'shy' => '',
+	'dependencies' => 'cms',
+	'conflicts' => '',
+	'priority' => '',
+	'module' => '',
+	'state' => 'stable',
+	'internal' => '',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearCacheOnLoad' => 1,
+	'lockType' => '',
+	'author_company' => '',
+	'private' => '',
+	'download_password' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.5.0-6.1.99',
+		),
+	),
+	'version' => '1.2.0',	// Don't modify this! Managed automatically during upload to repository.
+	'_md5_values_when_last_written' => 'a:25:{s:9:"ChangeLog";s:4:"1828";s:10:"README.txt";s:4:"ee2d";s:12:"ext_icon.gif";s:4:"1bdc";s:17:"ext_localconf.php";s:4:"860a";s:14:"ext_tables.php";s:4:"15a5";s:14:"ext_tables.sql";s:4:"01ca";s:24:"ext_typoscript_setup.txt";s:4:"884c";s:13:"locallang.php";s:4:"75bf";s:16:"locallang_db.php";s:4:"1ed9";s:14:"doc/manual.odt";s:4:"5516";s:14:"doc/manual.sxw";s:4:"fe6d";s:19:"doc/wizard_form.dat";s:4:"0ae2";s:20:"doc/wizard_form.html";s:4:"c0e6";s:30:"pi1/class.tx_timezones_pi1.php";s:4:"723f";s:22:"pi1/offsetinfo.inc.php";s:4:"5673";s:26:"pi1/offsetinfo.inc.php.bak";s:4:"3ee4";s:21:"pi1/timezones.inc.php";s:4:"3ff0";s:24:"pi1/update_timezones.php";s:4:"9a32";s:14:"pi2/ce_wiz.gif";s:4:"02b6";s:30:"pi2/class.tx_timezones_pi2.php";s:4:"9fee";s:38:"pi2/class.tx_timezones_pi2_wizicon.php";s:4:"b6f7";s:13:"pi2/clear.gif";s:4:"cc11";s:17:"pi2/locallang.php";s:4:"06ce";s:16:"pi2/template.tpl";s:4:"258e";s:24:"pi2/static/editorcfg.txt";s:4:"936e";}',
+);
+
+?>
